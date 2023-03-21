1356cf4d0f5c87e4f4bbd7bb09c6341bc39bbf3a
Darvin Cotrina <cc.darvin@gmail.com>
2023-03-21 17:06:51 -0500

    add events

---
+++ src/api/dc-event/controllers/dc-event.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dc-event controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dc-event.dc-event');
---
+++ src/api/dc-event/routes/dc-event.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dc-event router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::dc-event.dc-event');
