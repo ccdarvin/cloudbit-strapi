b2fd86b615d9d8baa3416de2355e92429d175ecb
Darvin Cotrina <cc.darvin@gmail.com>
2023-03-28 16:57:20 -0500

    add app to notes and events

--- src/api/dc-event/routes/dc-event.js
+++ src/api/dc-event/routes/dc-event.js
@@ -6,4 +6,19 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::dc-event.dc-event');
+module.exports = createCoreRouter('api::dc-event.dc-event', {
+    config: {
+        create: {
+            policies: ['global::currentApp', 'global::currentAppPost']
+        },
+        update: {
+            policies: ['global::currentApp']
+        },
+        find: {
+            policies: ['global::currentApp']
+        },
+        findOne: {
+            policies: ['global::currentApp']
+        },
+    } 
+});
--- src/api/dc-note/routes/dc-note.js
+++ src/api/dc-note/routes/dc-note.js
@@ -6,4 +6,19 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::dc-note.dc-note');
+module.exports = createCoreRouter('api::dc-note.dc-note', {
+    config: {
+        create: {
+            policies: ['global::currentApp', 'global::currentAppPost']
+        },
+        update: {
+            policies: ['global::currentApp']
+        },
+        find: {
+            policies: ['global::currentApp']
+        },
+        findOne: {
+            policies: ['global::currentApp']
+        },
+    }
+});
