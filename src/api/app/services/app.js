3c325f655afdf1c8cfb77851d5ecef30e8b31fdb
Darvin Cotrina <cc.darvin@gmail.com>
2023-04-28 15:44:00 -0500

    update to version 4.10.1

--- src/api/app/services/app.js
+++ src/api/app/services/app.js
@@ -6,4 +6,11 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::app.app');
+module.exports = createCoreService('api::app.app', ({ strapi,  }) => ({
+    async create(data) {
+        const result = await super.create(data)
+        console.log('create', result)
+        // create access
+        return result;
+    }
+}));
